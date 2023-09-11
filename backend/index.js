e49f8da64649431b9cd8d87575d82cf07d5ffc81
mariana <mariana.luciano@aluno.ifsp.edu.br>
2023-09-11 09:28:05 -0300

    alteracoes 1109

--- backend/index.js
+++ backend/index.js
@@ -25,3 +25,16 @@ app.get('/pessoa', async (req,res)=>{
     const consulta  = await getAllPessoas();
     return res.status(200).json(consulta);
 })
+
+app.get('/pessoa/:id', async (req,res)=>{
+    const {id} = req.params;
+    const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+    if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '})
+    return res.status(200).json(query);
+})
+
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body
+    const [query]= await connection.execute('insert into TestePessoa.Pessoa (nome,email) values (?,?)', [nome,email])
+    return query
+})
